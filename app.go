baf997edb5ed9e459f099fe84e7031518c500148
Blankeos <carloantonioct@gmail.com>
2024-02-03 01:01:05 +0800

    feat: initial project + playing around with it.

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// Get a random number
+func (a *App) VerifyAge(age int) string {
+	if age >= 18 {
+		return fmt.Sprintf("You are old enough.")
+	}
+
+	return fmt.Sprintf("You are NOT old enough!")
+}
