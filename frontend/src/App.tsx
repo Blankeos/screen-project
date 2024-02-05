458d5b04f68b7f312f6eb250cda5b6029a750b43
Blankeos <carloantonioct@gmail.com>
2024-02-05 20:21:18 +0800

    feat: Basic solid + wails example usage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRenderEffect, createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Greet } from "wailsjs/go/main/App";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
+
+  const [name, setName] = createSignal("");
+  const [greetResult, setGreetResult] = createSignal("");
+
+  function greet() {
+    Greet(name()).then((result) => setGreetResult(result));
+  }
 
   return (
     <>
@@ -16,6 +24,16 @@ function App() {
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
+      <input
+        name="name"
+        id="name"
+        placeholder="Name"
+        onInput={(e) => setName(e.target.value)}
+      />
+      <button onClick={greet}>Greet</button>
+      <br />
+      {greetResult()}
+      <hr />
       <h1>Vite + Solid</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,7 +47,7 @@ function App() {
         Click on the Vite and Solid logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import { resolve } from "node:path";
+
+//@ts-ignore
+const root = resolve(__dirname, ".");
 
 export default defineConfig({
   plugins: [solid()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(root, "src"),
+      wailsjs: resolve(root, "wailsjs"),
+    },
+  },
+});
